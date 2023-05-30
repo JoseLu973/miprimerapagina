8f12aa1bcc696070f3d7688c4b0f41ed2cf86998
JoseLu973 <joluort76@gmail.com>
2023-05-29 20:49:49 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function showAlert() {
+     alert('¡Hola esta es una Alerta de Javascript!'); }
+
+     document.querySelector("button.button-menu-toggle")
+     .addEventListener("click", function() {
+            document.querySelector(".nav-links").
+                       classList.toggle("nav-links-responsive")})
